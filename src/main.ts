cf70ada3a94597cad34691b065d09dba6b48d02c
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2025-10-21 16:23:00 +0700

    final?

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({ origin: 'http://localhost:3000' }); // Next.js frontend
+  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(4003);
 }
 bootstrap();
